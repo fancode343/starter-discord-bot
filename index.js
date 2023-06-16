1fb0e3839254cb21f7af2994b09cb515da89dfd6
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-16 12:43:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const TOKEN = process.env.TOKEN
 const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
 const GUILD_ID = process.env.GUILD_ID 
 
-const bedrock = require('bedrock-protocol') 
-const client = bedrock.createClient({ 
-host: 'MoonLightSMPG2.aternos.me', 
-port: 64319, 
-version: '1.20.0', 
-username: 'Bot22', 
-offline: true
-}) 
-
 console.log ('connected') 
 console.log ('Hit Control C If you want to stop')
 const axios = require('axios');
