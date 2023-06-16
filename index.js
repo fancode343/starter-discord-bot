d5a82319245839964be81e38a625613da5f01d12
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-16 12:33:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const TOKEN = process.env.TOKEN
 const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
 const GUILD_ID = process.env.GUILD_ID 
 
-const botbc = require('./bot.js')
-const axios = require('axios')
+const botbc = require('./bot.js');
+const axios = require('axios');
 const express = require('express');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
