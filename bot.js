4203a60d2a6debf03ffdbd6a7d65a6f48dc96dc4
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-16 12:36:15 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
+const bedrock = require('bedrock-protocol') 
+const client = bedrock.createClient({ 
+host: 'MoonLightSMPG2.aternos.me', 
+port: 64319, 
+version: '1.20.0', 
+username: 'Bot22', 
+offline: true
+}) 
+console.log ('connected') 
+console.log ('Hit Control C If you want to stop')
 
